aaaee1cdf98aedbd09e305376aa696a3b18793f3
Martin Moizard <martin.moizard@gmail.com>
2015-08-23 14:31:28 +0200

    Remove dist files

--- dist/src/App.js
+++
@@ -1,1 +0,0 @@
-React.render(React.createElement(Home, null), document.getElementById('app'));
--- dist/src/Home.js
+++
@@ -1,9 +0,0 @@
-var Home = React.createClass({displayName: "Home",
-  render: function(){
-    return (
-      React.createElement("div", null, 
-        React.createElement("div", null, " This is the home. ")
-      )
-    )
-  }
-});
